307a05dc19960a0bbfd5b96559e1627cab8b7397
moskil2 <tomasz.pieczara@gazeta.pl>
2026-06-22 19:19:40 +0200

    Change applicationId to app.spotrobotics.directdrop for Play Store

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.directdrop.app',
+  appId: 'app.spotrobotics.directdrop',
   appName: 'DirectDrop',
   webDir: 'dist'
 };
